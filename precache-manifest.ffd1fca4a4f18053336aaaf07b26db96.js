d74baf28c8c1caee0df585400acc35a90fade663
Phuoc Nguyen <phuocthien1996@gmail.com>
2020-01-17 10:38:13 +1000

    Updates

---
+++ precache-manifest.ffd1fca4a4f18053336aaaf07b26db96.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "860e3b45427de5cf1f66641c38fa04e3",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "c1c6731dbcb8b31ea5b8",
+    "url": "/contactmanager/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "ee81704a930778ca5931",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "c1c6731dbcb8b31ea5b8",
+    "url": "/contactmanager/static/js/2.3a767b07.chunk.js"
+  },
+  {
+    "revision": "53b91fb6912e14c9689b5be87306d93a",
+    "url": "/contactmanager/static/js/2.3a767b07.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ee81704a930778ca5931",
+    "url": "/contactmanager/static/js/main.2629cbfd.chunk.js"
+  },
+  {
+    "revision": "329d56e5d2a3841584cb",
+    "url": "/contactmanager/static/js/runtime-main.220e9dd0.js"
+  }
+]);
